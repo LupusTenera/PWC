10a849a1143e3ae0c6d7b61a741993c6d8e0784a
Fauve <fsetrodinomo@gmail.com>
2019-10-14 06:03:43 +0200

    progress

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <QtCore/qglobal.h>
+#if QT_VERSION >= 0x050000
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
+#else
+#endif
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <FelgoApplication>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app (argc, argv);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl) {
+            if (!obj && url == objUrl)
+                QCoreApplication::exit(-1);
+        }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
